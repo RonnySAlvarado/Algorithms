8b477dde1bc2ece57bad539c28ecf9cd880d00f9
Ronny Alvarado <rsalvarado777@gmail.com>
2019-12-12 22:38:50 -0600

    rps.py solved due to Artem showing us during instructor hour

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,31 @@
 
 import sys
 
+
+def recurse(prev, n, all_results):
+    if n == 0:
+        return prev
+
+    with_rock = recurse(prev + ['rock'], n - 1, all_results)
+    with_paper = recurse(prev + ['paper'], n - 1, all_results)
+    with_scissors = recurse(prev + ['scissors'], n - 1, all_results)
+
+    return [with_rock, with_paper, with_scissors]
+
+
 def rock_paper_scissors(n):
-  pass 
+    results = []
+    recurse([], n, results)
+
+    return results
 
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+rock_paper_scissors(1)
+rock_paper_scissors(2)
+rock_paper_scissors(3)
+# if __name__ == "__main__":
+#     if len(sys.argv) > 1:
+#         num_plays = int(sys.argv[1])
+#         print(rock_paper_scissors(num_plays))
+#     else:
+#         print('Usage: rps.py [num_plays]')
